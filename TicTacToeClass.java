9f4263b82f9158f03e5a18861f851dea2359e46d
Rusman313 <62064583+Rusman313@users.noreply.github.com>
2020-06-04 00:19:17 -0400

    Tests passed, added runner to see if code works properly and test it for myself.

--- TicTacToeClass.java
+++ TicTacToeClass.java
@@ -18,28 +18,47 @@ public class TicTacToeClass
 
 	// Accessor Methods
 
-	public boolean isWinner( char p )
-	{
-		return false;
-	}
+    public boolean isWinner(char p) {
+        for (int r = 0; r < 3; r++) {// will iterate through row numbers
+            if (p == board[r][0] && p == board[r][1] && p == board[r][2]) // will check horizontal win
+                return true;
+        }
+        for (int c = 0; c < 3; c++) {// will iterate through column numbers 
+            if (p == board[0][c] && p == board[1][c] && p == board[2][c])// will check for vertical win
+                return true;
+        }
+        if (p == board[0][2] && board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
+            return true;// will check diagonal win from bottom left to upper right
+        } else if (p == board[0][0] && board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
+            return true;// will check diagonal win from upper left to lower right
+        } else {
+            return false;// otherwise there is no winner
+        }
+    }
 
 	public boolean isFull()
 	{
+		if (turns >= 9){// turns are >= 9 then the board is full
+			return true;       
+		}
 		return false;
 	}
 
-	public boolean isCat()
-	{
+	public boolean isCat()//cat means tie
+	{// if the board is full and there is no winner its a tie
+		if (isFull() && !isWinner('X') && !isWinner('O')){
+			return true;
+		};
 		return false;	
 	}
 
-	public boolean isValid( int r, int c )
-	{
-		if ( 0 <= r && r <= 2 && 0 <= c && c <= 2 )
-			return true;
-		else
-			return false;
-	}
+    public boolean isValid(int r, int c) {
+        if (0 <= r && r <= 2 && 0 <= c && c <= 2) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 	public int numTurns()
 	{
@@ -66,7 +85,8 @@ public class TicTacToeClass
 
 	// Modifiers
 	public void playMove( char p, int r, int c )
-	{
+	{ board[r][c]=p;
+		turns++;
 	}
 
 }
